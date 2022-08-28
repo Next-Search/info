2a921f7dc360a56cfd3af783cca932c1e5de0434
Dave Mackey <dave@davemackey.net>
2022-08-28 11:19:57 -0400

    Update page header nav

---
+++ build/assets/js/255e2e42.7e368243.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknext_info=self.webpackChunknext_info||[]).push([[554],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,11 +69,10 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Something',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/next-search/',
             label: 'GitHub',
             position: 'right',
           },
